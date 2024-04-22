56768956cf9fb38deef31c57aa68f750c15ec5e2
Kahlium <kahligtz@gmail.com>
2024-04-22 15:36:03 -0600

    blog posts render function added

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -9,3 +9,18 @@ backButton.addEventListener('click', function (event)
     event.preventDefault()
     window.location.href = "index.html"
 })
+
+function renderPosts()
+{
+    for (let i = 0; i < currentPosts.length; i++)
+    {
+        const post = currentPosts[i]
+
+        const li = document.createElement('li');
+        li.textContent = post.title;
+
+        blogPostList.appendChild(li);
+    }
+}
+
+renderPosts();
